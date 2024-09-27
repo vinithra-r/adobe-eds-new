c768f2975359de359794e9c4093bb2d695e13c16
vinithra-r <60727571+vinithra-r@users.noreply.github.com>
2024-09-27 18:55:28 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    path.resolve(__dirname, 'scripts/wds.js'),
+  ],
+  output: {
+    filename: 'bundle.js', // Output file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery', // Makes jQuery available globally
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.css'],
+  },
+};
