10438861f11df051b7fd471aca52378258654eb0
vinithra-r <60727571+vinithra-r@users.noreply.github.com>
2024-10-10 11:44:36 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,5 +1,20 @@
-/*
- * Accordion Block
- * Include content on a page as a fragment.
- * https://www.aem.live/developer/block-collection/fragment
- */
+import { loadScript } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  // loadCSS('dist/bundle.css');
+  loadScript('dist/bundle.js');
+  loadScript('https://libs-europe.nissan-cdn.net/etc/designs/nissan-pace-vlp-plus/clientlibs-24.09.30.NISSAN-5/libs/analyticsManager.min.js');
+
+  const [heading, description] = block.querySelectorAll('p');
+  const wdsAccordian = document.createElement('div');
+  wdsAccordian.innerHTML = `<div class="accordion container">
+    <div class="row">
+      <div class="col-sm-2 col-md-12 col-l-12 col-xl-12">
+        <wds-accordion heading="${heading.innerHTML}" headingLevel=6>
+          <span slot="body">${description.innerHTML}</span>
+        </wds-accordion>
+      </div>
+    </div>
+  </div>`;
+  block.replaceWith(wdsAccordian);
+}
