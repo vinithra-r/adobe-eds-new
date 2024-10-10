392adfbebfcedc03ba0e80e965f421b171f75b47
vinithra-r <60727571+vinithra-r@users.noreply.github.com>
2024-10-10 14:51:38 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,6 +4,8 @@ export default function decorate(block) {
   loadScript('dist/bundle.js');
   loadScript('https://libs-europe.nissan-cdn.net/etc/designs/nissan-pace-vlp-plus/clientlibs-24.09.30.NISSAN-5/libs/analyticsManager.min.js');
   const isPublishMode = document.body.classList.contains('appear');
+  // eslint-disable-next-line no-console
+  console.log(isPublishMode);
   if (isPublishMode) {
     const [heading, description] = block.querySelectorAll('p');
     const wdsAccordian = document.createElement('div');
