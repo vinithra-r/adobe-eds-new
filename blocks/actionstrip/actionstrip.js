6938392c247eefd6074d16d42a85ec1635babae7
Vinithra <vinithra@publicissapient.com>
2024-10-14 12:02:56 +0530

    file updated

--- blocks/actionstrip/actionstrip.js
+++ blocks/actionstrip/actionstrip.js
@@ -2,21 +2,23 @@ import { loadScript } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   loadScript('https://libs-europe.nissan-cdn.net/etc/designs/nissan-pace-vlp-plus/clientlibs-24.09.30.NISSAN-5/libs/analyticsManager.min.js');
-  // eslint-disable-next-line no-console
-  console.log('test');
   const [heading, primaryBtn, secondaryBtn] = block.querySelectorAll('p');
-  const wdsActionStrip = document.createElement('div');
-  wdsActionStrip.innerHTML = `<div class="actionStrip">
-    <div class="row">
-      <div class="col-sm-2 col-md-12 col-l-12 col-xl-12">
-        <wds-action-strip heading="${heading.innerHTML}" background="grey">
-          <h2 slot="heading">${heading.innerHTML}</h2>
-          <wds-action-strip-button slot="buttons" buttontype="primary" background="light">${primaryBtn.innerHTML}</wds-action-strip-button>
-          <wds-action-strip-button slot="buttons" buttontype="secondary" background="light">${secondaryBtn.innerHTML}</wds-action-strip-button>
-        </wds-action-strip>
+  // eslint-disable-next-line no-console
+  console.log(heading.innerHTML);
+  if (heading.innerHTML) {
+    const wdsActionStrip = document.createElement('div');
+    wdsActionStrip.innerHTML = `<div class="actionStrip">
+      <div class="row">
+        <div class="col-sm-2 col-md-12 col-l-12 col-xl-12">
+          <wds-action-strip heading="${heading.innerHTML}" background="grey">
+            <h2 slot="heading">${heading.innerHTML}</h2>
+            <wds-action-strip-button slot="buttons" buttontype="primary" background="light">${primaryBtn.innerHTML}</wds-action-strip-button>
+            <wds-action-strip-button slot="buttons" buttontype="secondary" background="light">${secondaryBtn.innerHTML}</wds-action-strip-button>
+          </wds-action-strip>
+        </div>
       </div>
-    </div>
-  </div>`;
-  block.innerHTML = '';
-  block.appendChild(wdsActionStrip);
+    </div>`;
+    block.innerHTML = '';
+    block.appendChild(wdsActionStrip);
+  }
 }
