6db9c64acdd6bc1e375c496be000a4658480deb9
Vinithra <vinithra@publicissapient.com>
2024-10-14 10:39:19 +0530

    file updated

---
+++ blocks/actionStrip/actionStrip.js
@@ -0,0 +1,19 @@
+import { loadScript } from '../../scripts/aem.js';
+
+export default function decorate() {
+  loadScript('https://libs-europe.nissan-cdn.net/etc/designs/nissan-pace-vlp-plus/clientlibs-24.09.30.NISSAN-5/libs/analyticsManager.min.js');
+
+  // const [heading, description] = block.querySelectorAll('p');
+  // const wdsAccordian = document.createElement('div');
+  // wdsAccordian.innerHTML = `<div class="accordion">
+  //   <div class="row">
+  //     <div class="col-sm-2 col-md-12 col-l-12 col-xl-12">
+  //       <wds-accordion heading="${heading.innerHTML}" headingLevel=6>
+  //         <span slot="body">${description.innerHTML}</span>
+  //       </wds-accordion>
+  //     </div>
+  //   </div>
+  // </div>`;
+  // block.innerHTML = '';
+  // block.appendChild(wdsAccordian);
+}
