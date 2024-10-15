3102b165e84bac3aa0da90f842c482a78e1d851d
Vinithra <vinithra@publicissapient.com>
2024-10-15 12:04:41 +0530

    wds button block added

--- blocks/wds-button/wds-button.js
+++ blocks/wds-button/wds-button.js
@@ -1,4 +1,11 @@
 export default function decorate(block) {
   // eslint-disable-next-line no-console
   console.log(block);
+  const [btnLabel, btnType] = block.querySelectorAll('p');
+  const button = document.createElement('wds-button');
+  button.setAttribute('background', 'light');
+  button.setAttribute('variant', btnType);
+  button.setAttribute('iconSize', 'small');
+  button.textContent = btnLabel;
+  block.appendChild(button);
 }
