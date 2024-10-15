1ba4b7e31c85e89db4f696c667588a09d0f74328
Vinithra <vinithra@publicissapient.com>
2024-10-15 18:23:25 +0530

    wds button block added

--- blocks/actionstrip/actionstrip.js
+++ blocks/actionstrip/actionstrip.js
@@ -2,12 +2,12 @@ import { loadScript } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   loadScript('https://libs-europe.nissan-cdn.net/etc/designs/nissan-pace-vlp-plus/clientlibs-24.09.30.NISSAN-5/libs/analyticsManager.min.js');
-  const [heading, primaryBtn, secondaryBtn] = block.querySelectorAll('p');
+  const [heading, primaryBtn, secondaryBtn, theme] = block.querySelectorAll('p');
   const wdsActionStrip = document.createElement('div');
   wdsActionStrip.innerHTML = `<div class="actionstrip">
     <div class="row">
       <div class="col-sm-2 col-md-12 col-l-12 col-xl-12">
-        <wds-action-strip heading="${heading.innerHTML}" background="grey">
+        <wds-action-strip heading="${heading.innerHTML}" background="${theme ? theme.innerHTML : 'grey'}">
           <h2 slot="heading">${heading.innerHTML}</h2>
           <wds-action-strip-button slot="buttons" buttontype="primary" background="light">${primaryBtn.innerHTML}</wds-action-strip-button>
           <wds-action-strip-button slot="buttons" buttontype="secondary" background="light">${secondaryBtn.innerHTML}</wds-action-strip-button>
