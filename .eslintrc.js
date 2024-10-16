51ffee228aa6bf2285a1a76f5fedbf89bf1fc4d0
Vinithra <vinithra@publicissapient.com>
2024-10-16 13:16:15 +0530

    Hero model block added

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,8 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'xwalk/max-cells': ['error', {
+      heroModel: 10,
+    }],
   },
 };
